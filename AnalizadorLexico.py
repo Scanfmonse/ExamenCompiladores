dbf4e3fbf4ff9740ada12045d132d4a415be5eef
Sofia <114450205+Sofiana9024@users.noreply.github.com>
2025-04-04 20:45:40 -0600

    coment

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -8,7 +8,7 @@ cpp_keywords = {
     'catch', 'throw', 'namespace', 'using', 'include', 'define', 'template','cout',
     'endl','string', 'bool', 'using', 'iostream' ,'std', 'main'
 }
-
+#Comentario
 cpp_end = {
     ';','.',','
 }
