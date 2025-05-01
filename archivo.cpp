7251a32153615a64786e2510c36b7a6c8cf57d5d
Scanfmonse <monseestrada2001@gmail.com>
2025-05-01 12:23:38 -0600

    ultimos cambios

--- archivo.cpp
+++ archivo.cpp
@@ -1,3 +1,10 @@
-float suma(float a, float b) {
-    return a + b;
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    for (int i=0;i<5;i++)
+        for (int j=0;j<5;j++)
+            if(i==j)
+            cout<<"hola mundo"<<endl;
 }
